0730603f33e64756e16c97aa1e16db76e2e864c7
Carey Lzh <lzh.carey@gmail.com>
2024-11-20 14:34:53 +0800

    feat: add pseudo-random chosen prize amount

---
+++ main.py
@@ -0,0 +1,9 @@
+#import RPi.GPIO as RPi
+from time import sleep
+import random 
+total_prize_pool = 5.00
+while True:
+     probability_list = [0,0,0,0.05,0.05,0.05,0.05,0.05,0.05,0.15]
+     sleep(1)
+     chosen_prize = random.choice(probability_list)
+     print(chosen_prize)
