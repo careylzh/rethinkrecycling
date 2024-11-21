babb7b0622fb69c0b0c1bfe45a3221ecefeba251
Carey Lzh <lzh.carey@gmail.com>
2024-11-22 03:05:37 +0800

    feat: add loading page (circle animation) in Tkinter

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from time import sleep
 import random 
 total_prize_pool = 5.00
 while True:
+     #plays animation for loading
+     
+
+     #calculates reward
      probability_list = [0,0,0,0.05,0.05,0.05,0.05,0.05,0.05,0.15]
      sleep(1)
      chosen_prize = random.choice(probability_list)
      print(chosen_prize)
+
+     #displays win animations
