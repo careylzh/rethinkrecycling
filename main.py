9c9c596ba034757aad1ddc09529826a89dd1e90d
Carey Lzh <lzh.carey@gmail.com>
2024-12-06 14:36:57 +0800

    feat: add print statement in initiate crushing

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ def run_animations(animations):
 def initiate_crushing(x):
      global total_prize_pool
      total_prize_pool += 0.10
+     print("total prize pool", total_prize_pool)
 
 def initiate_gameplay(x):
      canvas.delete("all")
