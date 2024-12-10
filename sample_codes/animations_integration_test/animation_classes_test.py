7547f359506635c59ed932b9de4f54603fd9abc8
Carey Lzh <lzh.carey@gmail.com>
2024-12-10 20:14:03 +0800

    chore: remove unused sample animation

--- sample_codes/animations_integration_test/animation_classes_test.py
+++ sample_codes/animations_integration_test/animation_classes_test.py
@@ -1,29 +1,5 @@
 import tkinter as tk
 from time import sleep
-class AnimationA:
-    def __init__(self, canvas):
-        self.canvas = canvas
-        self.rect = None
-        self.dx = 5  # Movement per frame
-        self.running = False
-
-    def start(self, callback):
-        self.rect = self.canvas.create_rectangle(50, 50, 100, 100, fill="red")
-        self.running = True
-        self.callback = callback
-        self.animate()
-
-    def animate(self):
-        if not self.running:
-            return
-
-        self.canvas.move(self.rect, self.dx, 0)
-        x1, _, x2, _ = self.canvas.coords(self.rect)
-        if x2 > 400:  # Stop condition
-            self.running = False
-            self.callback()  # Trigger next animation
-        else:
-            self.canvas.after(50, self.animate)
 
 class SplashScreen:
     def __init__(self, canvas):
