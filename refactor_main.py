53eec5b36cfb79fac14704cb4e2b01debdf59bad
Carey Lzh <lzh.carey@gmail.com>
2024-12-11 19:26:04 +0800

    feat: refactor can display screen without while True, need to test if functions can be repeatedly called without while True

--- refactor_main.py
+++ refactor_main.py
@@ -9,6 +9,26 @@ from rewards import *
 # from animation_classes_library import *
 from phone_number_screen import PhoneNumberScreen
 
+def initiate_crushing(x):
+    global root
+    global canvas
+    global bg_label
+    print("user pushed lever. initiate_crushing called...\n")
+    bg_label.destroy()
+    current_background_image = ImageTk.PhotoImage(file=background_images[1])
+    bg_label = tk.Label(root, image=current_background_image)
+    bg_label.place(relwidth=1, relheight=1)
+
+    #  global total_prize_pool
+    #  total_prize_pool += 0.10
+    #  total_prize_pool = round(total_prize_pool,2)
+    #  print("total prize pool", total_prize_pool)
+    #  # update_text()
+
+def initiate_gameplay(x):
+    print("user pulled lever. initiate_gameplay called...\n")
+    global total_prize_pool
+
 #DEFINE states = 1,2,3,4,5 
 '''
 screens: 
@@ -19,88 +39,79 @@ screens:
 5
 
 '''
-SCREEN_STATE = 1
+# SCREEN_STATE = 1
 
-# import RPi.GPIO as GPIO
-# switch_in = 12 # to update
-# switch_in_crushing = 16
-# switch_in_for_redlight = 16
-# red_led_pin = 18
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(switch_in, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-# GPIO.setup(switch_in_crushing, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-# #GPIO.setup(switch_in_for_redlight, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-# GPIO.setup(red_led_pin, GPIO.OUT)
-# GPIO.output(red_led_pin, 0)
+import RPi.GPIO as GPIO
+switch_in = 12 # to update
+switch_in_crushing = 16
+switch_in_for_redlight = 16
+red_led_pin = 18
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(switch_in, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(switch_in_crushing, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setup(switch_in_for_redlight, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(red_led_pin, GPIO.OUT)
+GPIO.output(red_led_pin, 0)
 
 global total_prize_pool
 total_prize_pool = 5.00
+global root
+global canvas
+root = tk.Tk()
+root.wm_attributes('-fullscreen', 'True')
+canvas = tk.Canvas(root, width=1280, height=800, bg="white")
+canvas.pack()
+
+background_images = ["1-background.png","2-background.png","4-background.png","5-background-no-prize.png", "5-background-win.png"]
+
+GPIO.add_event_detect(switch_in, GPIO.RISING, callback=initiate_gameplay, bouncetime=500)
+GPIO.add_event_detect(switch_in_crushing, GPIO.RISING, callback=initiate_crushing, bouncetime=500)
+
+current_background_image = ImageTk.PhotoImage(file=background_images[0])
+global bg_label 
+bg_label = tk.Label(root, image=current_background_image)
+bg_label.place(relwidth=1, relheight=1)
 
 def initiate_crushing(x):
-     print("initiate_crushing called...\n")
+    global root
+    global canvas
+    global bg_label
+    print("user pushed lever. initiate_crushing called...\n")
+    bg_label.destroy()
+    current_background_image = ImageTk.PhotoImage(file=background_images[1])
+    bg_label = tk.Label(root, image=current_background_image)
+    bg_label.place(relwidth=1, relheight=1)
+
     #  global total_prize_pool
     #  total_prize_pool += 0.10
     #  total_prize_pool = round(total_prize_pool,2)
     #  print("total prize pool", total_prize_pool)
     #  # update_text()
-    #  GPIO.output(red_led_pin, 1)
 
 def initiate_gameplay(x):
-     print("initiate_gameplay called...\n")
-     global total_prize_pool
-    #  global bottle_count
-    #  if bottle_count != 0:
-    #       canvas.delete("after_submit_screen_butt")
-    #       canvas.delete("splash_screen_prize_pool_text")
-    #       calculate_reward_amount=calculate_reward()
-    #       animations = [
-    #                 # SplashScreen(canvas),
-    #                 LoadingScreen(canvas, calculate_reward_amount),
-    #                 PhoneNumberScreen(canvas, calculate_reward_amount, total_prize_pool, root),
-
-    #       ]
-    #       run_animations(animations)
-    #       bottle_count = 0
+    print("user pulled lever. initiate_gameplay called...\n")
+    global total_prize_pool
 
 #async interruptions which trigger screen animations by changing SCREEN_STATE - placing a bottle (ultrasonic) and    
 
-#main loop
-while True:
 
-    # GPIO.add_event_detect(switch_in, GPIO.RISING, callback=initiate_gameplay, bouncetime=500)
-    # GPIO.add_event_detect(switch_in_crushing, GPIO.RISING, callback=initiate_crushing, bouncetime=500)
+#insert bottle, sleep(2)
 
-    #init Tkinter root and canvas
-    global root
-    global canvas
-    root = tk.Tk()
-    root.wm_attributes('-fullscreen', 'True')
-    canvas = tk.Canvas(root, width=1280, height=800, bg="white")
-    canvas.pack()
+#CODE SNIPPET for each screen
+# current_background_image = ImageTk.PhotoImage(file=background_images[1])
+# bg_label = tk.Label(root, image=current_background_image)
+# bg_label.place(relwidth=1, relheight=1)
+# bg_label.destroy()
+# sleep(2)
 
-    background_images = ["1-background.png","2-background.png","4-background.png","5-background-no-prize.png", "5-background-win.png"]
+#no-prize
+current_background_image = ImageTk.PhotoImage(file=background_images[2])
+bg_label = tk.Label(root, image=current_background_image)
+bg_label.place(relwidth=1, relheight=1)
+# bg_label.destroy()
+sleep(2)
 
-    # current_background_image = ImageTk.PhotoImage(file=background_images[0])
-    # bg_label = tk.Label(root, image=current_background_image)
-    # bg_label.place(relwidth=1, relheight=1)
-    # bg_label.destroy()
-    # sleep(2)
-
-    #insert bottle, sleep(2)
-
-    current_background_image = ImageTk.PhotoImage(file=background_images[1])
-    bg_label = tk.Label(root, image=current_background_image)
-    bg_label.place(relwidth=1, relheight=1)
-    bg_label.destroy()
-    sleep(2)
-
-    #no-prize
-    current_background_image = ImageTk.PhotoImage(file=background_images[2])
-    bg_label = tk.Label(root, image=current_background_image)
-    bg_label.place(relwidth=1, relheight=1)
-    # bg_label.destroy()
-    sleep(2)
-
+root.mainloop()
     #win
     # current_background_image = ImageTk.PhotoImage(file=background_images[3])
     # bg_label = tk.Label(root, image=current_background_image)
@@ -144,4 +155,4 @@ while True:
 
     
 
-    root.mainloop()
+
