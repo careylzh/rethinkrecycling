976a70ae21203977a81e5e3138003f559e4c120f
Carey Lzh <lzh.carey@gmail.com>
2024-12-12 00:03:43 +0800

    feat: update initiate crushing in main.py

--- refactor_main.py
+++ refactor_main.py
@@ -9,22 +9,41 @@ from rewards import *
 # from animation_classes_library import *
 from phone_number_screen import PhoneNumberScreen
 
-def initiate_crushing(x):
-    global root
-    global canvas
-    global bg_label
-    print("user pushed lever. initiate_crushing called...\n")
-    bg_label.destroy()
-    current_background_image = ImageTk.PhotoImage(file=background_images[1])
-    bg_label = tk.Label(root, image=current_background_image)
-    bg_label.place(relwidth=1, relheight=1)
-    canvas.pack()
+# def initiate_crushing(x):
+#     global root
+#     global canvas
+#     global bg_label
+#     print("user pushed lever. initiate_crushing called...\n")
+#     bg_label.destroy()
+#     current_background_image = ImageTk.PhotoImage(file=background_images[1])
+#     bg_label = tk.Label(root, image=current_background_image)
+#     bg_label.place(relwidth=1, relheight=1)
+#     canvas.pack()
 
-    #  global total_prize_pool
-    #  total_prize_pool += 0.10
-    #  total_prize_pool = round(total_prize_pool,2)
-    #  print("total prize pool", total_prize_pool)
-    #  # update_text()
+#     #  global total_prize_pool
+#     #  total_prize_pool += 0.10
+#     #  total_prize_pool = round(total_prize_pool,2)
+#     #  print("total prize pool", total_prize_pool)
+#     #  # update_text()
+
+def initiate_crushing(x):
+    # global root
+    # global canvas
+    # global bg_label
+    print("user pushed lever. initiate_crushing called...\n")
+    # bg_label.destroy()
+    # OLD
+    # bg_label.destroy()
+    # current_background_image = ImageTk.PhotoImage(file=background_images[1])
+    # bg_label = tk.Label(root, image=current_background_image, tags="bg_label")
+    # bg_label.place(relwidth=1, relheight=1)
+    # bg_label.pack()
+
+    new_image = Image.open("5-background-win.png").resize((1280, 800))
+    new_tk_image = ImageTk.PhotoImage(new_image)
+    bg_label.configure(image=new_tk_image)
+    bg_label.image = new_tk_image  # Update reference to avoid garbage collection
+
 
 def initiate_gameplay(x):
     print("user pulled lever. initiate_gameplay called...\n")
@@ -59,9 +78,9 @@ GPIO.output(red_led_pin, 0)
 # global root
 # global canvas
 root = tk.Tk()
-root.wm_attributes('-fullscreen', 'True')
-canvas = tk.Canvas(root, width=1280, height=800, bg="white")
-canvas.pack()
+# root.wm_attributes('-fullscreen', 'True')
+# canvas = tk.Canvas(root, width=1280, height=800, bg="white")
+# canvas.pack()
 
 background_images = ["1-background.png","2-background.png","4-background.png","5-background-no-prize.png", "5-background-win.png"]
 
@@ -75,23 +94,6 @@ bg_label.pack()
 # bg_label.place(relwidth=1, relheight=1)
 # bg_label.pack()
 
-def initiate_crushing(x):
-    # global root
-    # global canvas
-    # global bg_label
-    print("user pushed lever. initiate_crushing called...\n")
-    # bg_label.destroy()
-    # OLD
-    # bg_label.destroy()
-    # current_background_image = ImageTk.PhotoImage(file=background_images[1])
-    # bg_label = tk.Label(root, image=current_background_image, tags="bg_label")
-    # bg_label.place(relwidth=1, relheight=1)
-    # bg_label.pack()
-
-    new_image = Image.open("5-background-win.png").resize((1280, 800))
-    new_tk_image = ImageTk.PhotoImage(new_image)
-    bg_label.configure(image=new_tk_image)
-    bg_label.image = new_tk_image  # Update reference to avoid garbage collection
 
     #  global total_prize_pool
     #  total_prize_pool += 0.10
