8477e7d07add23154f5b898aaf85bbc46f3df287
Carey Lzh <lzh.carey@gmail.com>
2024-12-14 01:52:49 +0800

    chore: remove unused code

--- refactor_main.py
+++ refactor_main.py
@@ -9,22 +9,7 @@ from rewards import *
 # from animation_classes_library import *
 from phone_number_screen import PhoneNumberScreen
 import threading
-# def initiate_crushing(x):
-#     global root
-#     global canvas
-#     global bg_label
-#     print("user pushed lever. initiate_crushing called...\n")
-#     bg_label.destroy()
-#     current_background_image = ImageTk.PhotoImage(file=background_images[1])
-#     bg_label = tk.Label(root, image=current_background_image)
-#     bg_label.place(relwidth=1, relheight=1)
-#     canvas.pack()
 
-#     #  global total_prize_pool
-#     #  total_prize_pool += 0.10
-#     #  total_prize_pool = round(total_prize_pool,2)
-#     #  print("total prize pool", total_prize_pool)
-#     #  # update_text()
 i=0
 total_prize_pool = 5.00
 
@@ -44,15 +29,13 @@ def initiate_push_instructions():
     print("Bottle detected. waiting for user to push lever...\n")
 
     for i in range(0,3):
-        # i+=1
         print("tracking i in push: ", i)
         new_image = Image.open(background_images[i])
         new_tk_image = ImageTk.PhotoImage(new_image)
         bg_label.configure(image=new_tk_image)
         bg_label.image = new_tk_image  # Update reference to avoid garbage collection
         sleep(0.5)
-    # if(i==2):
-    #     i=0
+
     return
 
 def initiate_push(x):
@@ -71,8 +54,7 @@ def initiate_push(x):
             bg_label.configure(image=new_tk_image)
             bg_label.image = new_tk_image  # Update reference to avoid garbage collection
             sleep(0.5)
-        # if(i==2):
-        #     i=0
+
         pull_instruction_on = 1
     pacman_active = 0
     return
@@ -114,7 +96,6 @@ def run_us_sensor():
 
             if (distance < 11 and push_instruction_on == 0) :
                 sleep(1)
-                # initiate_push()
                 initiate_push_instructions()
                 push_instruction_on = 1
 
@@ -137,12 +118,6 @@ def initiate_pull(x):
         
         print("user pulled lever. initiate_gameplay called...\n")     
 
-        # global total_prize_pool
-        # new_image = Image.open(background_images[9])
-        # new_tk_image = ImageTk.PhotoImage(new_image)
-        # bg_label.configure(image=new_tk_image)
-        # bg_label.image = new_tk_image
-
         for i in range(13,15):
             # i+=1
             print("tracking i in pull: ", i)
@@ -164,8 +139,7 @@ def initiate_pull(x):
             new_tk_image = ImageTk.PhotoImage(new_image)
             bg_label.configure(image=new_tk_image)
             bg_label.image = new_tk_image  # Update reference to avoid garbage collection
-        # global i
-        # i=9
+
         sleep(2)
         new_image = Image.open(background_images[0]) #show default bg screen
         new_tk_image = ImageTk.PhotoImage(new_image)
@@ -176,15 +150,6 @@ def initiate_pull(x):
         lever_active = 0
         pull_instruction_on = 0
         
-    return
-
-    # sleep(2)
-    # new_image = Image.open("2-background-no-prize.png").resize((1280, 800))
-    # new_tk_image = ImageTk.PhotoImage(new_image)
-    # bg_label.configure(image=new_tk_image)
-    # bg_label.image = new_tk_image
-
-#DEFINE states = 1,2,3,4,5 
     return
 '''
 screens: 
@@ -209,22 +174,9 @@ GPIO.setup(switch_in_crushing, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(red_led_pin, GPIO.OUT)
 GPIO.output(red_led_pin, 0)
 
-# global total_prize_pool
-# total_prize_pool = 5.00
-# global root
-# global canvas
 root = tk.Tk()
 root.wm_attributes('-fullscreen', 'True')
-# canvas = tk.Canvas(root, width=1280, height=800, bg="black")
-# canvas.pack()
 
-# background_images = ["1-background.png","2-background.png","4-background.png","5-background-no-prize.png", "5-background-win.png"]
-# background_images = [
-#     f"test{i}.png" for i in range(1, 24)
-# ]
-# background_images = [
-#     "Artboard 1 copy.png","Artboard1.png"
-# ]
 background_images = [
     f"{i}.png" for i in range(1, 24)
 ]
@@ -234,58 +186,16 @@ GPIO.add_event_detect(switch_in, GPIO.RISING, callback=initiate_push, bouncetime
 GPIO.add_event_detect(switch_in_crushing, GPIO.RISING, callback=initiate_pull, bouncetime=500)
 
 current_background_image = ImageTk.PhotoImage(file=background_images[0])
-# global bg_label 
 bg_label = tk.Label(root, image=current_background_image)
 bg_label.pack()
-# bg_label.place(relwidth=1, relheight=1)
-# bg_label.pack()
 
-
-    #  global total_prize_pool
-    #  total_prize_pool += 0.10
-    #  total_prize_pool = round(total_prize_pool,2)
-    #  print("total prize pool", total_prize_pool)
-    #  # update_text()
-
-# def initiate_gameplay(x):
-#     print("user pulled lever. initiate_gameplay called...\n")
-#     global total_prize_pool
-
-#async interruptions which trigger screen animations by changing SCREEN_STATE - placing a bottle (ultrasonic) and    
-
-
-#insert bottle, sleep(2)
-
-#CODE SNIPPET for each screen
-# current_background_image = ImageTk.PhotoImage(file=background_images[1])
-# bg_label = tk.Label(root, image=current_background_image)
-# bg_label.place(relwidth=1, relheight=1)
-# bg_label.destroy()
-# sleep(2)
-
-#no-prize
-# current_background_image = ImageTk.PhotoImage(file=background_images[2])
-# bg_label = tk.Label(root, image=current_background_image)
-# bg_label.place(relwidth=1, relheight=1)
-# bg_label.pack()
-# bg_label.destroy()
-# sleep(2)
-
-### Us sensor initialisation
 sensor_thread = threading.Thread(target=run_us_sensor)
 sensor_thread.start()
 
 root.mainloop()
-    #win
-    # current_background_image = ImageTk.PhotoImage(file=background_images[3])
-    # bg_label = tk.Label(root, image=current_background_image)
-    # bg_label.place(relwidth=1, relheight=1)
-    # bg_label.destroy()
 
 
-   #in each of the screens below, add and delete the following images
-
-##uncomment if you need to revise the PROGRAM FLOW
+##READ the code below if you need to revise the PROGRAM FLOW
     # if SCREEN_STATE == 1: 
     #     pass  
     #     #always create elements with a tag
